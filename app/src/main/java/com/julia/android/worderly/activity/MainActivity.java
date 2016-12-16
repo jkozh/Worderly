f74a20ec8a28fb5fd9882460ac184fc85536b542
julia <jkozhukhovskaya@gmail.com>
2016-12-15 21:35:16 -0800

    feat: Add an Auth instance variables in the MainActivity class

--- app/src/main/java/com/julia/android/worderly/activity/MainActivity.java
+++ app/src/main/java/com/julia/android/worderly/activity/MainActivity.java
@@ -3,9 +3,15 @@ package com.julia.android.worderly.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.julia.android.worderly.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Firebase instance variables
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
