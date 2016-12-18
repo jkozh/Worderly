232c5a6e50ea77f808a2aa2691fda05ae876a38e
julia <jkozhukhovskaya@gmail.com>
2016-12-18 02:34:04 -0800

    feat: Add a User model class

---
+++ app/src/main/java/com/julia/android/worderly/model/User.java
@@ -0,0 +1,17 @@
+package com.julia.android.worderly.model;
+
+public class User {
+
+    public String username;
+    public String email;
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+}
