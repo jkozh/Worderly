b5165fd2570e380291bb517f5d9faeacd725ea63
Julia Kozhukhovskaya <julia.kozh@yandex.ru>
2017-02-11 00:30:36 -0800

    feat: Add modifications in WordListAdapter

--- app/src/main/java/com/julia/android/worderly/ui/game/dragdrop/WordListAdapter.java
+++ app/src/main/java/com/julia/android/worderly/ui/game/dragdrop/WordListAdapter.java
@@ -3,6 +3,7 @@ package com.julia.android.worderly.ui.game.dragdrop;
 import android.app.Activity;
 import android.content.ClipData;
 import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -15,11 +16,12 @@ import com.julia.android.worderly.R;
 
 import java.util.List;
 
-public class WordListAdapter extends RecyclerView.Adapter<ListViewHolder> {
+public class WordListAdapter extends RecyclerView.Adapter<ListViewHolder> implements View.OnTouchListener {
 
+    private static final String TAG = WordListAdapter.class.getSimpleName();
     private List<CustomList> mCustomList;
     private Listener mListener;
-    private boolean colorWhite = false;
+    private int colorTile;
 
     public WordListAdapter(List<CustomList> customList, Listener listener) {
         this.mCustomList = customList;
@@ -31,58 +33,57 @@ public class WordListAdapter extends RecyclerView.Adapter<ListViewHolder> {
         View view = LayoutInflater.from(
                 parent.getContext()).inflate(R.layout.row_item, parent, false);
         if (parent.getId() == R.id.recycler_view_top) {
-            colorWhite = true;
+            colorTile = R.color.blue_grey_100;
         }
         DisplayMetrics dm = new DisplayMetrics();
-        ((Activity)parent.getContext()).getWindowManager().getDefaultDisplay().getMetrics(dm);
-        view.setLayoutParams(
-                new RecyclerView.LayoutParams((dm.widthPixels-20) / 7, (dm.widthPixels-20) / 7));
+        ((Activity) parent.getContext()).getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int tileSize = (dm.widthPixels - 20) / 7;
+        view.setLayoutParams(new RecyclerView.LayoutParams(tileSize, tileSize));
         return new ListViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ListViewHolder holder, int position) {
         CustomList customList = mCustomList.get(position);
-        if (colorWhite) {
-            holder.rl.setBackgroundColor(holder.itemView.getResources().getColor(R.color.blue_grey_100));
-        } else {
-            holder.rl.setBackgroundColor(holder.itemView.getResources().getColor(customList.color));
+        if (colorTile != R.color.blue_grey_100) {
+            colorTile = customList.color;
         }
+        holder.rl.setBackgroundColor(
+                ContextCompat.getColor(holder.itemView.getContext(), colorTile));
         holder.textTile.setText(String.valueOf(customList.letter));
         holder.textScore.setText(String.valueOf(customList.value));
         holder.rl.setTag(position);
-        holder.rl.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                switch(motionEvent.getAction()){
-                    case MotionEvent.ACTION_DOWN:
-                        ClipData data = ClipData.newPlainText("", "");
-                        View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                            view.startDragAndDrop(data, shadowBuilder, view, 0);
-                        } else {
-                            view.startDrag(data, shadowBuilder, view, 0);
-                        }
-                        return true;
-                }
-                return false;
-            }
-        });
+        holder.rl.setOnTouchListener(this);
         holder.rl.setOnDragListener(new DragListener(mListener));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCustomList.size();
+    }
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                ClipData data = ClipData.newPlainText("", "");
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    v.startDragAndDrop(data, shadowBuilder, v, 0);
+                } else {
+                    v.startDrag(data, shadowBuilder, v, 0);
+                }
+                return true;
+        }
+        return false;
     }
 
     public DragListener getDragInstance() {
         if (mListener != null) {
             return new DragListener(mListener);
         } else {
-            Log.e("WordListAdapter", "Listener wasn't initialized!");
+            Log.e(TAG, "Listener wasn't initialized!");
             return null;
         }
-    }
-
-    @Override
-    public int getItemCount() {
-        return mCustomList.size();
     }
 
     List<CustomList> getCustomList() {
@@ -92,5 +93,6 @@ public class WordListAdapter extends RecyclerView.Adapter<ListViewHolder> {
     void updateCustomList(List<CustomList> customList) {
         this.mCustomList = customList;
     }
+
 }
 
